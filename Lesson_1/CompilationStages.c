e8cf148450afe3cb456409cd637f7b89a1e53e47
Sree Raghuveer Pedapudi <sreeraghuveerpedapudi@gmail.com>
2025-07-01 13:21:59 -0500

    code updated

--- Lesson_1/CompilationStages.c
+++ Lesson_1/CompilationStages.c
@@ -26,15 +26,100 @@
 //Example for Preprocessor functionalities
 #include<stdio.h>
 
+<<<<<<< HEAD
 //#define PI 3.147
 
 void main(void)
 {
+=======
+#define PI 3.147
+void Arthmetic(int NUM_1,int NUM_2);
+void Bitwise(int NUM_1,int NUM_2);
+int main(void)
+{
+	/*
+>>>>>>> 538e370 (code updated)
 	printf("Testing compilation stages\n");
 #ifdef PI
 	printf("PI value is %f\n", PI);
 #else
 	printf("PI is not defined\n");
 #endif
+<<<<<<< HEAD
 		
 }
+=======
+	*/
+	/*
+	int Rad,Result;
+	printf("Enter radius of circle\n");
+	scanf("%d",&Rad);
+	Result=2*PI*Rad;
+	printf("Perimeter of a circle%d\n",Result);
+	return 0;
+	*/
+
+	int NUM_1,NUM_2,OPT;
+		printf("Enter your choice\n");
+		printf("1)Arthmetic\n");
+		printf("2)Bitwise\n");
+	//	printf("3)Logical\n");
+	        
+		scanf(" %d",&OPT);
+		printf("Enter two numbers\n\n");
+		scanf("%d %d",&NUM_1,&NUM_2);
+	switch(OPT)
+	{
+		case 1:
+			printf("EnterArthmetic\n");
+			Arthmetic(NUM_1,NUM_2);
+			break;
+			
+		case 2:
+                        Bitwise(NUM_1,NUM_2);
+			break;
+			/*
+		case 'l':
+                        printf("Multiplication of two numbers:%d\n",NUM_1+NUM_2);
+                        break;
+			*/
+		
+	}
+	return 0;
+}
+	void Arthmetic(int NUM_1,int NUM_2)
+	{
+		char choice;
+		printf("Add,Sub,Multi,Divi:\n");
+		scanf(" %c",&choice);
+
+		switch(choice)
+		{
+			case 'a' :
+                        printf("Addition of two numbers:%d\n",NUM_1+NUM_2);
+                        break;
+                	case 'b':
+                        printf("Subtraction of two numbers:%d\n",NUM_1+NUM_2);
+                        break;
+                	case 'c':
+                        printf("Multiplication of two numbers:%d\n",NUM_1+NUM_2);
+                        break;
+                	case 'd':
+                        printf("Division of two numbers:%d\n",NUM_1+NUM_2);
+                        break;
+                	default:
+                        break;
+		}
+	}
+	void Bitwise(int NUM_1,int NUM_2)
+	{
+		printf("AND Operations:%d\n",NUM_1&NUM_2);
+		 printf("OR Operations:%d\n",NUM_1|NUM_2);
+		  printf("XOR Operations:%d\n",NUM_1^NUM_2);
+		   printf("NOT Operations:%d %d\n",~NUM_1,~NUM_2);
+		    printf("LEFT Operations:%d\n",NUM_1<<NUM_2);
+		     printf("RIGHT Operations:%d\n",NUM_1>>NUM_2);
+	}	
+
+
+>>>>>>> 538e370 (code updated)
